392214ef89c17486cd2b106125a35002ed93c44f
higor <h.vitall96@gitlab.com>
2021-02-15 00:53:46 -0300

    desafio completo

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,26 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository
+  ){
+
+     }
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
     // TODO
+    const custumerExists = await this.customersRepository.findByEmail(email);
+
+    if(custumerExists){
+      throw new AppError('e-mai already assigned');
+    }
+
+    const custumer = await this.customersRepository.create({name, email});
+
+    return custumer;
+
   }
 }
 
